591d64cf0f1c0372b3a234f7035d4e3b6c826711
심상갑 <kappa2410@gmail.com>
2024-10-23 13:48:59 +0900

    Initial Commit

---
+++ GabAnythingLab/GabAnythingLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GabAnythingLabApp.swift
+//  GabAnythingLab
+//
+//  Created by Gab on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GabAnythingLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GabAnythingLabTests/GabAnythingLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  GabAnythingLabTests.swift
+//  GabAnythingLabTests
+//
+//  Created by Gab on 10/23/24.
+//
+
+import Testing
+@testable import GabAnythingLab
+
+struct GabAnythingLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
