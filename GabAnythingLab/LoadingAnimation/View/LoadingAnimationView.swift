158ddb8b6b694f4445cc047b1db44c0b552d71bb
심상갑 <kappa2410@gmail.com>
2024-11-01 10:06:18 +0900

    Feat: Path를 이용한 로딩애니메이션 만들어보자

--- GabAnythingLab/LoadingAnimation/View/LoadingAnimationView.swift
+++ GabAnythingLab/LoadingAnimation/View/LoadingAnimationView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 import Combine
 
 struct LoadingAnimationView: View {
-    @State var timer = Timer.publish(every: 1, on: .current, in: .default, options: nil)
+    @State var timer = Timer.publish(every: 1.5, on: .current, in: .default, options: nil)
     
     @State private var cancellables: Set<AnyCancellable> = []
     
     @State private var condition: Bool = true
     
-    @State private var count: Int = .zero
+    @State private var count: CGFloat = .zero
+    @State private var count2: CGFloat = .zero
+    
+    @State private var degree = 0.0
     
     @State private var testCancellable: Cancellable?
     
@@ -26,37 +29,83 @@ struct LoadingAnimationView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        main2
+    }
+    
+    
+    @ViewBuilder
+    var main1: some View {
+        path
             .onAppear {
-                let cancellable = timer
-                    .sink { _ in
-                        
-                    }
-                
-                cancellables.insert(cancellable)
-                
-                testCancellable = timer.connect()
+//                setTimer()
             }
-            .onReceive(timer) { output in
-                print("상갑 logEvent \(#function) output: \(output)")
-                count += 1
-                if count == 10 {
-                    print("상갑")
+            .onReceive(timer) { _ in
+                if count >= 1 {
+                    count = .zero
                     testCancellable?.cancel()
+                    return
                 }
+                
+                count += 0.1
             }
-            .onChange(of: cancellables) { oldValue, newValue in
-                print("상갑 logEvent \(#function) : \(newValue)")
+            .onChange(of: count) { newValue in
+                print("상갑 logEvent \(#function) count: \(newValue)")
             }
-        
-        Rectangle()
-            .fill(.mint)
-            .frame(width: 30, height: 30)
-            .onTapGesture {
-                timer = Timer.publish(every: 1, on: .current, in: .default, options: nil)
-                count = .zero
-                testCancellable = timer.connect()
+    }
+    
+    @ViewBuilder
+    var main2: some View {
+        path2
+    }
+    
+    @ViewBuilder
+    var path: some View {
+        Path { path in
+            path.addArc(center: CGPoint(x: 100, y: 100),
+                        radius: 30,
+                        startAngle: .degrees(0),
+                        endAngle: .degrees(360),
+                        clockwise: true)
+            
+        }
+        .trim(from: 0, to: count)
+        .stroke(.mint, lineWidth: 2)
+        .animation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true))
+    }
+    
+    @ViewBuilder
+    var path2: some View {
+        Path { path in
+            
+            path.addArc(center: CGPoint(x: 150, y: 150),
+                        radius: 30,
+                        startAngle: .degrees(0),
+                        endAngle: .degrees(360),
+                        clockwise: true)
+        }
+        .trim(from: count, to: count2)
+        .stroke(Color.mint, lineWidth: 5)
+//        .rotationEffect(.degrees(360))
+        .animation(.easeInOut(duration: 1.5))
+        .onAppear {
+            setTimer()
+        }
+        .onReceive(timer) { output in
+            if count2 == .zero {
+                count2 = 1
+            } else {
+                count = 1
             }
+        }
+    }
+    
+    private func setTimer() {
+        if testCancellable == nil {
+            testCancellable = self.timer.connect()
+        } else {
+            self.timer = Timer.publish(every: 1, on: .current, in: .default, options: nil)
+            testCancellable = self.timer.connect()
+        }
     }
 }
 
